b4c3d425b6a2e6b0ec550c85acd57f9b1370d0a8
Stephan Winzer <stephanwinzer1990@gmail.com>
2023-12-15 11:27:47 +0100

    no message

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -70,10 +70,33 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza funghi"
+        ingredients="Tomato, mozarella, mushrooms, and ricotta cheese"
+        photoName="pizzas/funghi.jpg"
+        price={15}
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
   );
 }
 
@@ -89,16 +112,6 @@ function Footer() {
   console.log(checkOpen);
 
   return <footer className="footer">{checkOpen}</footer>;
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
