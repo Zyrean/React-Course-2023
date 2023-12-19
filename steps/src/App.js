436412fed98570972dd447427ae2efde5e16741d
Stephan Winzer <stephanwinzer1990@gmail.com>
2023-12-19 23:37:29 +0100

    no message

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,22 +7,24 @@ const messages = [
 ];
 
 export default function App() {
-  let step = 1;
+  const [step, setStep] = useState(1);
 
   const handlePrevious = function () {
-    step--;
+    if (step > 1) setStep(step - 1);
+    console.log(step);
   };
 
   const handleNext = function () {
-    step++;
+    if (step < 3) setStep(step + 1);
+    console.log(step);
   };
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
